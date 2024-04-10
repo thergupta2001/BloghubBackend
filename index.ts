6dc4fe7d9a3d4da45cc981a0bf282d672ed2acef
Rohan G <2001.rohangupta@gmail.com>
2024-04-10 18:24:53 +0530

    Sequelize initialize

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import express, { Request, Response } from 'express'
+import express, { Application, Request, Response } from 'express'
 
-const app = express();
+const app: Application = express();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello from TS yay")
